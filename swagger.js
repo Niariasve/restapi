396684f31cebdd4c43887930a645c2192af67c2d
Niariasve <138833714+Niariasve@users.noreply.github.com>
2024-07-15 13:52:41 -0500

    Camnbio de host

--- swagger.js
+++ swagger.js
@@ -5,7 +5,10 @@ const doc = {
     "title": 'REST API',
     "description": 'REST API with Express and Firestore'
   },
-  "host": 'localhost:5500',
+  "host": 'https://restapi-1-9ohq.onrender.com/',
+  "schemes": [
+    "https"
+  ],
   "basePath": "/api",
 };
 
